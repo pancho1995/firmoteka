2d8f3e37361e2bdd03183c1a5c485d2bcde8db46
mynameisalmir <blsw@outlook.com>
2021-04-15 22:11:46 +0200

    Cognito with Postconfirm function

--- backend/functions/confirm-user-signup.js
+++ backend/functions/confirm-user-signup.js
@@ -1,37 +1,42 @@
-// const DynamoDB = require('aws-sdk/clients/dynamodb');
-// const DocumentClient = new DynamoDB.DocumentClient()
-// const Chance = require('chance');
-// const chance = new Chance();
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DocumentClient = new DynamoDB.DocumentClient();
+const Chance = require("chance");
+const chance = new Chance();
 
-// const { USERS_TABLE } = process.env
+const { USERS_TABLE } = process.env;
 
 exports.handler = async (event) => {
-    if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
-        // const name = event.request.userAttributes['name'];
-        // const suffix = chance.string({ length: 8, casing: 'upper', alpha: true, numeric: true });
-        // const screenName = `${name.replace(/[^a-zA-Z0-9]/g, "")}${suffix}`;
-        // const user = {
-        //     id: event.userName,
-        //     name,
-        //     screenName,
-        //     createdAt: new Date().toJSON(),
-        //     followersCount: 0,
-        //     followingCount: 0,
-        //     tweetsCount: 0,
-        //     likesCounts: 0
-        // };
+    if (event.triggerSource === "PostConfirmation_ConfirmSignUp") {
+        const user_id = chance.string({
+            length: 8,
+            casing: "upper",
+            alpha: true,
+            numeric: true,
+        });
+        const username = event.userName;
+        const full_name = event.request.name;
+        const phone_number = event.request.phone_number;
+        const email = event.request.email;
 
-        // console.log(user);
-        console.log(event);
+        var fullDateArr = new Date().toISOString().split("T");
+        const creationTime = fullDateArr[0] + " " + fullDateArr[1].split(".")[0];
 
-        // await DocumentClient.put({
-        //     TableName: USERS_TABLE,
-        //     Item: user,
-        //     ConditionExpression: 'attribute_not_exists(id)'
-        // }).promise()
+        const user = {
+            user_id: user_id,
+            username: username,
+            full_name: full_name,
+            phone_number: phone_number,
+            email: email,
+            created_at: creationTime,
+            updated_at: creationTime,
+            company_id: 'NULL',
+        };
 
-        return event;
-    } else {
-        return event;
+        await DocumentClient.put({
+            TableName: USERS_TABLE,
+            Item: user,
+            ConditionExpression: "attribute_not_exists(id)",
+        }).promise();
     }
+    return event;
 };
